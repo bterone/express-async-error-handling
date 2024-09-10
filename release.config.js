8683863f83e992c1b628e0f199138abc636d58e3
Terone <bterone@gmail.com>
2024-09-10 09:18:58 +0700

    feat: add release config file

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branches: [{ name: 'master' }],
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github"
+    ]
+};
