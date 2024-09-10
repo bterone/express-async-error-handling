95c8f21d0bea2b42ee296e6b85b6c81ef7f50738
Terone <bterone@gmail.com>
2024-09-10 09:45:10 +0700

    feat: We are the ones creating our releases now

--- release.config.js
+++ release.config.js
@@ -1,8 +1,6 @@
 module.exports = {
     branches: [{ name: 'main' }],
     "plugins": [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/github"
+        "@semantic-release/commit-analyzer"
     ]
 };
